95074326115db564bdae88374197186244d75462
Dario Piotrowicz <dario@cloudflare.com>
2023-06-15 21:58:11 +0100

    implement gateway worker to glue things together

--- apps/gateway-worker/src/worker.ts
+++ apps/gateway-worker/src/worker.ts
@@ -1,32 +1,24 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
+	NODE_APP_BASE_URL: string;
+	NEXT_ON_PAGES_BASE_URL: string;
 }
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		const nextOnPagesResponse = await redirectRequest(request, env.NEXT_ON_PAGES_BASE_URL);
+		if (nextOnPagesResponse.status !== 404) {
+			console.log(`${new URL(request.url).pathname} - Request handled at the edge`);
+			return nextOnPagesResponse;
+		}
+
+		console.log(`${new URL(request.url).pathname} - Request handled by node app`);
+		return redirectRequest(request, env.NODE_APP_BASE_URL);
 	},
 };
+
+async function redirectRequest(request: Request, targetBaseUrl: string): Promise<Response> {
+	const url = new URL(request.url);
+	const nextOnPagesUrl = new URL(`${targetBaseUrl}${url.pathname}`);
+
+	return fetch(new Request(nextOnPagesUrl, request));
+}
